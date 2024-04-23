d3b54da5dc9ad58112f4860b72d62a1f94527cc8
sahil-313 <sahilshakeel3103@gmail.com>
2024-04-23 18:55:11 +0530

    Defined URL's for the 'Streamer' app

--- streamer/urls.py
+++ streamer/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     # Example: path('route/', views.view_function, name='route_name'),
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("signup", views.signup, name="signup"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
 ]
