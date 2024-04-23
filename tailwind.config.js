7ffa383fe59350d08a79f6d5dab6456203eb4efc
sahil-313 <sahilshakeel3103@gmail.com>
2024-04-23 18:50:21 +0530

    Added more colours to the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './**/templates/**/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black-dark': '#00000050',
+        'dull-white': '#FFFFFFB3',
+        'white-light': '#FFFFFF30',
+        'white-medium': '#FFFFFF40',
+        'neon-blue': '#2FB8FF',
+      }
+    },
   },
   plugins: [],
 }
