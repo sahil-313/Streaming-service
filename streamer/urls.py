d4e47f5b41b395b600f776dc88790fcc0f0eee20
sahil-313 <sahilshakeel3103@gmail.com>
2024-04-28 15:55:14 +0530

    Update urls.py

--- streamer/urls.py
+++ streamer/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("signup", views.signup, name="signup"),
     path("profile/<int:user_id>", views.profile, name="profile"),
+    path("upload", views.upload_video, name="upload_video"),
+    path("watch/<int:video_id>", views.watch_video, name="watch_video"),
+
+    # API routes
+    path("get_videos", views.get_videos, name="get_videos")
 ]
